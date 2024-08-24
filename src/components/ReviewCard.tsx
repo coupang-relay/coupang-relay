3a5d1868587269e67beb97bcbeeb7e98aff1f8b1
Junho Yeo <i@junho.io>
2024-08-24 22:17:45 +0900

    Fix layout shift in `ReviewCard`

--- src/components/ReviewCard.tsx
+++ src/components/ReviewCard.tsx
@@ -10,12 +10,16 @@ type ReviewCardProps = {
 }
 export const ReviewCard: React.FC<ReviewCardProps> = ({ id }) => {
   return (
-    <Link href={`/relay/${id + 1}`} key={id} className="flex-shrink-0 w-[180px] mx-1 snap-center">
+    <Link
+      href={`/relay/${id + 1}`}
+      key={id}
+      className="w-[180px] min-h-[269px] max-h-[269px] overflow-hidden mx-1 snap-center rounded-[8px]"
+    >
       <Container>
         <Image
           src={`/img/relay/${id + 1}.jpeg`}
           alt="cat"
-          className="w-full h-full object-cover"
+          className="w-full h-full object-cover absolute top-0 left-0 right-0 bottom-0"
           width={180}
           height={320}
         />
@@ -31,7 +35,7 @@ export const ReviewCard: React.FC<ReviewCardProps> = ({ id }) => {
 
 const Container = styled.li`
   width: 100%;
-  height: 269px;
+  height: 100%;
   position: relative;
   background: rgba(214, 214, 214, 0.5);
   border-radius: 8px;
